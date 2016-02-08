afac0e495bac013a8192ce34f610c0163a72b061
phazhion <phazhion@gmail.com>
2016-02-08 14:42:58 -0600

    Main Design

--- sanfra-bb.js
+++ sanfra-bb.js
@@ -1,23 +1,55 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  // This code only runs on the client
+  Template.body.helpers({
+    fielders: [
+      { name: "Jardinero Izq.",
+        clase: "lf col-md-1 col-xs-1" 
+      }, 
+      { name: "Jardinero CenI",
+        clase: "cf col-md-1 col-md-offset-2 col-xs-1 col-xs-offset-2"
+      }, 
+      { name: "Jardinero CenD",
+        clase: "cf col-md-1 col-md-offset-4 col-xs-1 col-xs-offset-4"
+      }, 
+      { name: "Jardinero Der.",
+        clase: "rf col-md-1 col-md-offset-2 col-xs-1 col-xs-offset-2"
+      }
+    ], 
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    toppers: [
+      { name: "Shortstop",
+        clase: "ss col-md-1 col-md-offset-4 col-xs-1 col-xs-offset-4"
+      },
+      { name: "2nda Base",
+        clase: "2b col-md-1 col-md-offset-2 col-xs-1 col-xs-offset-2"
+      }
+    ],
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+    bottomers: [
+      { name: "3era Base",
+        clase: "3b col-md-1 col-md-offset-2 col-xs-1 col-xs-offset-2"
+      }, 
+      { name: "Pitcher",
+        clase: "pi col-md-1 col-md-offset-2 col-xs-1 col-xs-offset-2"
+      },
+      { name: "1era Base",
+        clase: "1b col-md-1 col-md-offset-2 col-xs-1 col-xs-offset-2"
+      }
+    ],
+      
+    catcher: [ 
+      { name: "Catcher",
+        clase: "ca col-md-1 col-md-offset-6 col-xs-1 col-xs-offset-6"
+      }
+    ], 
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+    teams: [
+      {name: "San Francisco"}, 
+      {name: "Aguilas"}
+    ], 
+
+    numIn: 2, 
+    upDown: "arriba", 
+    outs: 2
   });
 }
